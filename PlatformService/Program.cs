6be8fffe192e0be060a12a09a2a7480f32ff8cbd
Oscar Calles <ocalles@gmail.com>
2023-06-06 17:11:16 +0200

    Command yaml added

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseInMemoryDatabase("Inmemory"));
 
 // Add services to the container.
+
 builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 
@@ -33,7 +34,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
